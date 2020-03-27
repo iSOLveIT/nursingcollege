83b7b90c2597c151491f03bc63b3a81beffd84ce
iSOLveIT <isolveitgroup@gmail.com>
2020-03-27 19:40:40 +0000

    Initial commit

--- package/routes.py
+++ package/routes.py
@@ -9,10 +9,10 @@ app.add_url_rule("/", view_func=IndexEndpoint.as_view("index"))
 app.add_url_rule("/dashboard", view_func=DashboardEndpoint.as_view("dashboard"))
 
 # Route for My Courses
-app.add_url_rule("/dashboard/courses/<string:_id>", view_func=CoursesEndpoint.as_view("courses"))
+app.add_url_rule("/courses/<string:course_code>", view_func=CoursesEndpoint.as_view("courses"))
 
 # Route for Examdetails
-app.add_url_rule("/dashboard/courses/examdetails", view_func=ExamdetailsEndpoint.as_view("examdetails"))
+app.add_url_rule("/courses/<string:course_code>/examdetails/<string:exam_code>", view_func=ExamdetailsEndpoint.as_view("examdetails"))
 
 # Route for Exam
-app.add_url_rule("//dashboard/courses/examdetails/exam", view_func=ExamEndpoint.as_view("exam"))
+app.add_url_rule("/courses/<string:course_code>/examdetails/<string:exam_code>/exam", view_func=ExamEndpoint.as_view("exam"))
