94bc1c7d7e3728a7462a008cd6a8a835c2645dad
iSOLveIT <isolveitgroup@gmail.com>
2020-04-02 12:12:34 +0000

    Test Commit

--- package/__init__.py
+++ package/__init__.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 # Check Configuration section for more details
 SESSION_COOKIE_NAME = "pantangcollege"
-SESSION_TYPE = 'filesystem'
+#SESSION_TYPE = 'filesystem'
 SESSION_COOKIE_HTTPONLY = False
 SESSION_KEY_PREFIX = 'pcollege'
-SESSION_FILE_DIR = "app_session"
+#SESSION_FILE_DIR = "app_session"
 app.config.from_object(__name__)
 Session(app)
 
